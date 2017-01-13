a4298888d7fd496db578863358d5b66e7f3ed578
Gaston Pompe <gastonpompe@client.openvpn.net>
2017-01-12 23:14:06 -0500

    Initial Commit

---
+++ practicearea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practicearea
+//
+//  Created by Gaston Pompe on 2017-01-12.
+//  Copyright © 2017 Gaston Pompe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
